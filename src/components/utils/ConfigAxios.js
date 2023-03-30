41110a5ce0af2b76703cd06c87a631c74908063b
msaebi031 <m.saebi9@gmail.com>
2023-03-30 18:36:52 +0430

    v2

--- src/components/utils/ConfigAxios.js
+++ src/components/utils/ConfigAxios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Set config defaults when creating the instance
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.LOCALHOST_URL,
   headers: {
     Authorization: process.env.AUTH_TOKEN,
     "Content-Type": "application/json",
